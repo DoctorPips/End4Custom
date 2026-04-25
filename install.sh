bcdd5b72519cba8b88808cb72d99452cf61df080
drpips <dr.p1ps@proton.me>
2026-04-25 14:38:20 +0300

    Install Script Release

--- install.sh
+++ install.sh
@@ -2,21 +2,16 @@
 set -e
 
 REPO="https://github.com/DoctorPips/End4Custom.git"
+REPO_NAME="End4Custom"
 INSTALL_DIR="$HOME/.config/hypr"
 CONFIG_DIR="$HOME/.config/hypr/custom"
 
-if [ -d "$CONFIG_DIR" ]; then
-	echo "Custom folder already exists."
+echo -e "\e[34mInstalling End4 custom config...\e[31m"
 
-	read -p "Overwrite it? (y/n): " choice
-
-	if [[ "$choice" != "y" && "$choice" != "Y"]]; then
-	echo "Aborted."
-	exit 0
-	fi
-
-fi
-
-echo "Installing Hyprland config..."
-
+mkdir -p "$INSTALL_DIR/custom/scripts"
 git clone "$REPO"
+mv "$REPO_NAME"/custom/*.conf "$INSTALL_DIR/custom"
+mv "$REPO_NAME"/custom/scripts/*.sh "$INSTALL_DIR/custom/scripts"
+rm -rf "$REPO_NAME"
+
+echo -e "\e[32mInstallation Success!\e[0m"
